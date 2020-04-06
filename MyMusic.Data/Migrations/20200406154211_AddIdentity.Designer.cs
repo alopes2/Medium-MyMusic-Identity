d256aef55b3ae332cdb94fdd75758c877a04e888
Andre Lopes <andrelo@dunnhumby.com>
2020-04-06 18:49:47 +0200

    Add migration to configure identity

--- MyMusic.Data/Migrations/20200406154211_AddIdentity.Designer.cs
+++ MyMusic.Data/Migrations/20200406154211_AddIdentity.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MyMusic.Data;
 
 namespace MyMusic.Data.Migrations
 {
     [DbContext(typeof(MyMusicDbContext))]
-    partial class MyMusicDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200406154211_AddIdentity")]
+    partial class AddIdentity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- MyMusic.Data/MyMusicDbContext.cs
+++ MyMusic.Data/MyMusicDbContext.cs
@@ -1,10 +1,14 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using MyMusic.Core.Models;
+using MyMusic.Core.Models.Auth;
 using MyMusic.Data.Configurations;
 
 namespace MyMusic.Data
 {
-    public class MyMusicDbContext : DbContext
+    public class MyMusicDbContext : IdentityDbContext<User, Role, Guid>
     {
         public DbSet<Music> Musics { get; set; }
         public DbSet<Artist> Artists { get; set; }
@@ -15,6 +19,8 @@ namespace MyMusic.Data
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
+            base.OnModelCreating(builder);
+            
             builder
                 .ApplyConfiguration(new MusicConfiguration());
 
